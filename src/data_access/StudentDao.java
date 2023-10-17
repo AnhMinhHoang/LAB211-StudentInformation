52849c1aa577d6670be80b3414b8fd5043e1fce4
GoldCandy <GoldCandy@GauDan>
2023-10-17 09:43:30 +0700

    Done

---
+++ src/data_access/StudentDao.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package data_access;
+
+import common.Library;
+import common.Validation;
+import java.util.Collections;
+import java.util.List;
+import model.Student;
+
+/**
+ *
+ * @author GoldCandy
+ */
+public class StudentDao {
+
+    private static StudentDao instance = null;
+    protected Validation valid = new Validation();
+
+    public static StudentDao Instance() {
+        if (instance == null) {
+            synchronized (StudentDao.class) {
+                if (instance == null) {
+                    instance = new StudentDao();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public void adding(List<Student> students, Student new_student) {
+        new_student.setName(valid.inputString("Enter name"));
+        new_student.setClasses(valid.inputString("Enter class"));
+        new_student.setMark(valid.inputInt("Enter mark", 0, 100));
+        students.add(new_student);
+        Collections.sort(students, new Library());
+    }
+
+    public void display(List<Student> students) {
+        int count = 1;
+        for (Student student : students) {
+            System.out.println("-------------Student " + count+"-------------");
+            System.out.println(student.toString());
+            count++;
+        }
+    }
+}
